e3825ca85d02705778ba274d5a8d837ccabb473a
bielyGit <biely@o2.pl>
2020-11-16 18:05:12 +0100

    Initial_CheckIn

---
+++ src/main/java/com/wannaboulder/WannaBoulderAppApplication.java
@@ -0,0 +1,13 @@
+package com.wannaboulder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannaBoulderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannaBoulderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wannaboulder/WannaBoulderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wannaboulder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannaBoulderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
